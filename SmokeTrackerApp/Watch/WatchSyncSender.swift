f932b220784055dc81d47732319dbe35a75e0349
batu <batu.kizmazoglu@oerotrading.com>
2026-05-31 19:06:15 +0300

    feat(watch): yerel +1 kaydı, bugünkü sayı ekranı ve gönderim

---
+++ SmokeTrackerApp/Watch/WatchSyncSender.swift
@@ -0,0 +1,22 @@
+import Foundation
+import WatchConnectivity
+import SmokeTrackerCore
+import SmokeTrackerData
+
+/// Yeni olayları WCSession ile iPhone'a güvenilir biçimde aktarır.
+final class WatchSyncSender: NSObject, WCSessionDelegate {
+    override init() {
+        super.init()
+        guard WCSession.isSupported() else { return }
+        WCSession.default.delegate = self
+        WCSession.default.activate()
+    }
+
+    /// Tek bir olayı sıraya koyar; karşı taraf erişilemezse bile teslim edilir.
+    func send(_ event: SmokingEvent) {
+        guard let data = try? SyncMessageCodec.encode([event]) else { return }
+        WCSession.default.transferUserInfo(["payload": data])
+    }
+
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {}
+}
