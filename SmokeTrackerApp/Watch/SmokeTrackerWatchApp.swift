2852bc7fc429ea443b48e28e37d4e34ca05237a6
batu <batu.kizmazoglu@oerotrading.com>
2026-05-31 19:03:34 +0300

    feat(app): XcodeGen projesi + iskelet iOS/watchOS app hedefleri

---
+++ SmokeTrackerApp/Watch/SmokeTrackerWatchApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct SmokeTrackerWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("SmokeTracker")
+        }
+    }
+}
---
+++ SmokeTrackerApp/iOS/SmokeTrackerApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct SmokeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("SmokeTracker")
+        }
+    }
+}
